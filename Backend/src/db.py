3087fa0b4ec0741eb37e78c69a211845d477731b
lukman41 <lukmanmoyo1@gmail.com>
2022-05-06 20:00:59 -0400

    m

--- Backend/src/db.py
+++ Backend/src/db.py
@@ -4,6 +4,10 @@ from sqlalchemy import Integer, DateTime
 from flask_login import UserMixin
 import datetime
 from mimetypes import guess_extension, guess_type
+import hashlib
+import os
+import bcrypt
+
 db = SQLAlchemy()
 
 ins_assoc_table = db.Table(
@@ -74,13 +78,59 @@ class User(db.Model):
     rate = db.Column(db.String, nullable=True)
     profile_pic = db.Column(db.String,nullable = True)
 
+    password_digest = db.Column(db.String, nullable=False)
+
+    # Session information
+    session_token = db.Column(db.String, nullable=False, unique=True)
+    session_expiration = db.Column(db.DateTime, nullable=False)
+    update_token = db.Column(db.String, nullable=False, unique=True)
+
     def __init__(self, **kwargs):
         """
         Initializes User object
         """
         self.name = kwargs.get("name", "")
         self.email = kwargs.get("email", "")
-        #self.profile_pic = kwargs.get("profile_pic")
+        self.profile_pic = kwargs.get("profile_pic")
+        self.password_digest = bcrypt.hashpw(kwargs.get("password").encode("utf8"), bcrypt.gensalt(rounds=13))
+        self.renew_session()
+
+
+    def _urlsafe_base_64(self):
+        """
+        Randomly generates hashed tokens (used for session/update tokens)
+        """
+        return hashlib.sha1(os.urandom(64)).hexdigest()
+
+    def renew_session(self):
+        """
+        Renews the sessions, i.e.
+        1. Creates a new session token
+        2. Sets the expiration time of the session to be a day from now
+        3. Creates a new update token
+        """
+        self.session_token = self._urlsafe_base_64()
+        self.session_expiration = datetime.datetime.now() + datetime.timedelta(days=1)
+        self.update_token = self._urlsafe_base_64()
+
+    def verify_password(self, password):
+        """
+        Verifies the password of a user
+        """
+        return bcrypt.checkpw(password.encode("utf8"), self.password_digest)
+
+    def verify_session_token(self, session_token):
+        """
+        Verifies the session token of a user
+        """
+        return session_token == self.session_token and datetime.datetime.now() < self.session_expiration
+
+    def verify_update_token(self, update_token):
+        """
+        Verifies the update token of a user
+        """
+        return update_token == self.update_token
+
 
     def serialize(self):   
         """
